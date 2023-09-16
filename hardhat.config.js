eded5c196f9e387961cde0bed48e47775b19cbc0
Rakeally <40327144+Rakeally@users.noreply.github.com>
2023-09-16 20:03:26 +0100

    escrow hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,23 @@
-require('@nomicfoundation/hardhat-toolbox');
+// require('@nomicfoundation/hardhat-toolbox');
+
+// module.exports = {
+//   solidity: "0.8.17",
+//   paths: {
+//     artifacts: "./app/src/artifacts",
+//   }
+// };
+
+
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.17",
-  paths: {
-    artifacts: "./app/src/artifacts",
-  }
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111
+    },
+  },
+
 };
